f848947c62814a03c050d4d52ad32a9fdf06a9c4
Zhiwei35 <zhiwei.huang@intel.com>
2023-12-20 00:03:49 -0800

    fix cal padding offset ut

--- src/kernels/cal_paddingoffset.h
+++ src/kernels/cal_paddingoffset.h
@@ -3,6 +3,7 @@
 #include <cuda.h>
 #include <cuda_fp16.h>
 #include "src/utils/macro.h"
+#include "src/utils/tensor.h"
 
 void launchCalPaddingoffset(TensorWrapper<int>* padding_offset, 
                             TensorWrapper<int>* cum_seqlens,
--- tests/unittests/test_cal_paddingoffset.cu
+++ tests/unittests/test_cal_paddingoffset.cu
@@ -26,22 +26,17 @@ int main() {
     h_padding_offset = (int*)malloc(sizeof(int) * batch_size * max_q_len);
     cudaMalloc((void**)&d_padding_offset, sizeof(int) * batch_size * max_q_len);
 
-    int* h_pinned_token_num;
-    cudaMallocHost((void**)&h_pinned_token_num, sizeof(int));
-
     for(int i = 0; i < batch_size; i++) { // 3
        h_seq_lens[i] = batch_size;
     }
     cudaMemcpy(d_seq_lens, h_seq_lens, sizeof(int) * batch_size, cudaMemcpyHostToDevice);
+    TensorWrapper<int>* padding_offset = new TensorWrapper<int>(Device::GPU, type_int, {batch_size, max_q_len}, d_padding_offset);
+    TensorWrapper<int>* cum_seqlens = new TensorWrapper<int>(Device::GPU, type_int, {batch_size + 1}, d_cum_seqlens);
+    TensorWrapper<int>* input_lengths = new TensorWrapper<int>(Device::GPU, type_int, {batch_size}, d_seq_lens);
     // debug info, better to retain: std::cout << "before launch kernel" << std::endl;
-    int h_token_num{};
-    launchCalPaddingoffset(h_pinned_token_num, 
-                           &h_token_num, 
-                           d_padding_offset, 
-                           d_cum_seqlens,
-                           d_seq_lens, 
-                           batch_size,
-                           max_q_len);
+    launchCalPaddingoffset(padding_offset, 
+                           cum_seqlens,
+                           input_lengths);
     // debug info, better to retain: std::cout << "after launch kernel" << std::endl;
     // Note: remember to memcpy from device to host and define the correct copy size(mul the sizeof(dtype)), or will cause segment fault
     cudaMemcpy(h_padding_offset, d_padding_offset, sizeof(int) * batch_size * max_q_len, cudaMemcpyDeviceToHost);
@@ -60,7 +55,6 @@ int main() {
     free(h_seq_lens);
     free(h_padding_offset);
     free(h_cum_seqlens);
-    cudaFreeHost(h_pinned_token_num);
     cudaFree(d_seq_lens);
     cudaFree(d_padding_offset);
     cudaFree(d_cum_seqlens);
