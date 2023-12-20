192cdff9b2f524c48284634e9bfd96a9abe5cd4c
Zhiwei35 <zhiwei.huang@intel.com>
2023-12-19 22:25:46 -0800

    add weight convert script and cal paddingoffset

---
+++ src/kernels/cal_paddingoffset.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <cuda_runtime.h>
+#include <cuda.h>
+#include <cuda_fp16.h>
+#include "src/utils/macro.h"
+
+void launchCalPaddingoffset(TensorWrapper<int>* padding_offset, 
+                            TensorWrapper<int>* cum_seqlens,
+                            TensorWrapper<int>* input_lengths //actual input lens
+);
