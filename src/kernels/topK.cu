21cb29d43fc394164c7fa779f65f761903c95fc4
Zhiwei35 <zhiwei.huang@intel.com>
2024-01-06 06:39:53 -0800

    add template for topK

--- src/kernels/topK.cu
+++ src/kernels/topK.cu
@@ -6,9 +6,9 @@
 #include <cub/cub.cuh>
 
 // Note: a b两个topK reduce输出一个topK
-template<int K>
-__device__ topK<K> reduce_functor(const topK<K>& a, const topK<K>& b) {
-    topK<K> res = a;
+template<typename T, int K>
+__device__ topK<T, K> reduce_functor(const topK<T, K>& a, const topK<T, K>& b) {
+    topK<T, K> res = a;
     for(int i = 0; i < K; i++){
         res.insertHeap(b.val[i], b.id[i]);
     }
@@ -17,11 +17,11 @@ __device__ topK<K> reduce_functor(const topK<K>& a, const topK<K>& b) {
 // gridsize:bs * beamwidth * BlockPerBeam 
 // blocksize:256
 // shape infer: [bs, beamwidth, vocab size] => [bs, beamwidth, BlockPerBeam, K]
-template<int K, int blockSize, int BlockPerBeam>
+template<typename T, int K, int blockSize, int BlockPerBeam>
 __global__ void topK_kernel_round1(const float* probs, const int vocab_size, 
                                          int* topK_ids, float* topK_vals)
 {
-    typedef cub::BlockReduce<topK<K>, blockSize> blockreduce;
+    typedef cub::BlockReduce<topK<T, K>, blockSize> blockreduce;
     __shared__ typename blockreduce::TempStorage temp_storage;
 
     int tid = threadIdx.x;
@@ -29,7 +29,7 @@ __global__ void topK_kernel_round1(const float* probs, const int vocab_size,
     int gid = blockIdx.x * blockDim.x + threadIdx.x;
     int row_id = bid / BlockPerBeam;
     int block_lane = bid % BlockPerBeam;
-    topK<K> thread_topK;
+    topK<T, K> thread_topK;
     thread_topK.init();
     // thread local reduce
     for(int data_id = tid + block_lane * blockSize; data_id < vocab_size; data_id += BlockPerBeam * blockSize){
@@ -39,7 +39,7 @@ __global__ void topK_kernel_round1(const float* probs, const int vocab_size,
        //thread_topK.insertHeap(data, data_id); // bug
     }
     //block local reduce
-    topK<K> block_topK = blockreduce(temp_storage).Reduce(thread_topK, reduce_functor<K>);
+    topK<T, K> block_topK = blockreduce(temp_storage).Reduce(thread_topK, reduce_functor<K>);
 
     if(tid == 0){
         for(int k_offset = 0; k_offset < K; k_offset++) {
@@ -50,29 +50,29 @@ __global__ void topK_kernel_round1(const float* probs, const int vocab_size,
         }
     }
 }
-// shape infer: [bs, beamwidth, BlockPerBeam, K] => [bs, K]
+// shape infer: [bs, beamwidth, BlockPerBeam, K] => [bs, beamwidth, K]
 // ids是beamwidth * vocalsize中的全局word id
 // gridSize = bs
 // blockSize = 256
-template<int K, int blockSize, int BlockPerBeam>
+template<typename T, int K, int blockSize, int BlockPerBeam>
 __global__ void topK_kernel_round2(const int* topK_ids, const float* topK_vals,
                                     int* final_topK_ids, float* final_topK_vals)
 {
-    typedef cub::BlockReduce<topK<K>, blockSize> blockreduce;
+    typedef cub::BlockReduce<topK<T, K>, blockSize> blockreduce;
     __shared__ typename blockreduce::TempStorage temp_storage;
 
     int tid = threadIdx.x;
     int bid = blockIdx.x;
     int gid = blockIdx.x * blockDim.x + threadIdx.x;
     int row_id = bid;
-    topK<K> thread_topK;
+    topK<T, K> thread_topK;
     // thread local reduce    
     for(int i = tid; i < BlockPerBeam * K; i += blockDim.x) {
         int data_offset = bid * BlockPerBeam * K + i;
         thread_topK.insertHeap(topK_vals[data_offset], topK_ids[data_offset]);
     }
     // block reduce
-    topK<K> block_topK = blockreduce(temp_storage).Reduce(thread_topK, reduce_functor<K>);
+    topK<T, K> block_topK = blockreduce(temp_storage).Reduce(thread_topK, reduce_functor<T, K>);
     if(tid == 0){
         for(int k_offset = 0; k_offset < K; k_offset++) {
             // topK_vals[row_id * vocab_size + block_lane * blockSize + k_offset] = block_topK.val[k_offset]; //bug
@@ -81,14 +81,14 @@ __global__ void topK_kernel_round2(const int* topK_ids, const float* topK_vals,
         }
     }    
 }
-
-void launchTopKforBeamSearch(const float* probs, 
+template<typename T>
+void launchTopKforBeamSearch(const T* probs, 
                             const int batch_size,
                             const int vocab_size, 
                             int* topk_ids,
-			    float* topk_vals,
+			    T* topk_vals,
 			    int* final_topk_ids,
-			    float* final_topk_vals) {// GPU workspace is for intermediate buffer and output buffer
+			    T* final_topk_vals) {// GPU workspace is for intermediate buffer and output buffer
     constexpr int BlockPerBeam = 8;
     constexpr int beamwidth = 1;
     constexpr int K = 5;
@@ -97,9 +97,9 @@ void launchTopKforBeamSearch(const float* probs,
     int topK_ids_buf_size = batch_size * beamwidth * BlockPerBeam * K;
     int final_topK_val_buf_size = batch_size * beamwidth * K;
     // memory plan
-    float* topK_vals = topk_vals;
+    T* topK_vals = topk_vals;
     int* topK_ids = topk_ids;
-    float* final_topK_vals = final_topk_vals;
+    T* final_topK_vals = final_topk_vals;
     int* final_topK_ids = final_topk_ids;    
     // prepare launch
     // TODO: add GPUconfig API to easily get GPU config, ep: maxblocknums
@@ -115,9 +115,9 @@ void launchTopKforBeamSearch(const float* probs,
     dim3 grid_round2(BlockNums2);
     dim3 block_round2(256);
     // debug info, better to retain: std::cout << "in cu file, before launch" << std::endl;
-    topK_kernel_round1<K, 256, BlockPerBeam>
+    topK_kernel_round1<T, K, 256, BlockPerBeam>
                         <<<grid_round1, block_round1>>>(probs, vocab_size, topK_ids, topK_vals);
-    topK_kernel_round2<K, 256, BlockPerBeam>
+    topK_kernel_round2<T, K, 256, BlockPerBeam>
                         <<<grid_round2, block_round2>>>(topK_ids, topK_vals, final_topK_ids, final_topK_vals);
     // debug info, better to retain: std::cout << "in cu file, after launch" << std::endl;
 }
--- src/kernels/topK.h
+++ src/kernels/topK.h
@@ -1,27 +1,29 @@
+#pragma once
 #include <cuda_runtime.h>
 #include <float.h>
-template<int K>
+template<typename T, int K>
 struct topK
 {
-    float val[K];
+    T val[K];
     int id[K];
 
     __device__ void init(){
         for (int i = 0; i < K; i++) {
             id[i] = -1;
-            val[i] = FLT_MIN;
+            val[i] = -1e-20;
         }
     }
 
-    __device__ void insertHeap(float data, int data_id){
-        if(id[K-1] == -1 || val[K-1] < data){
+    __device__ void insertHeap(T data, int data_id){
+        float v = (float)val[K-1];
+        if(id[K-1] == -1 || v < (float)data){
             id[K-1] = data_id;
             val[K-1] = data;
         }
         //Note: 仅需一轮冒泡排序（插入新元素的重排），因为此时除了最后一个新元素，其它都是有序
         for (int i = K - 2; i >= 0; i--){
             if(val[i + 1] > val[i] || id[i] == -1) {
-                float tmp = val[i];
+                T tmp = val[i];
                 val[i] = val[i + 1];
                 val[i + 1] = tmp;                
                 int tmp_id = id[i];
